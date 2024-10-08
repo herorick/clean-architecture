1bbbcc8a589a2830186e9df826d25befa81e637a
Nhat Nguyen <nhatnguyen.anh@zenfolio.com>
2024-10-08 16:01:30 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-import express from "express";
-import dotenv from "dotenv";
+import express from 'express';
+import dotenv from 'dotenv';
 dotenv.config();
 
-import { bookRoutes } from "./interface/routes/bookRoutes";
-import { errorHandler } from "./interface/middleware/errorHandler";
-import { logger } from "./infrastructure/logger";
-import { setupSwagger } from "./interface/swagger";
-import { userRoutes } from "./interface/routes/UserRoutes";
+import { bookRoutes } from './interface/routes/bookRoutes';
+import { errorHandler } from './interface/middleware/errorHandler';
+import { logger } from './infrastructure/logger';
+import { setupSwagger } from './interface/swagger';
+import { userRoutes } from './interface/routes/UserRoutes';
 
 const app = express();
 
 app.use(express.json());
-app.use("/api", bookRoutes);
-app.use("/api", userRoutes);
+app.use('/api', bookRoutes);
+app.use('/api', userRoutes);
 app.use(errorHandler);
 setupSwagger(app);
 
@@ -20,4 +20,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   logger.info(`Server is running on port ${PORT}`);
 });
-
